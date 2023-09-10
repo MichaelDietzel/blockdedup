dd971161d81b77e0fca68eb5a2d68c6dedf7e90d
Michael Dietzel <14862324+MichaelDietzel@users.noreply.github.com>
2023-09-10 19:41:20 +0200

    added deduplication for the found matches

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,46 @@ use std::io::BufReader;
 use std::io::SeekFrom;
 use std::io::prelude::*;
 use crc::{Crc, CRC_64_ECMA_182};
+use std::os::unix::io::{RawFd, AsRawFd};
+use errno::errno;
+use std::os::raw::{c_int, c_ulong};
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct file_dedupe_range_info
+{
+    dest_fd: i64,
+    dest_offset: u64,
+    bytes_deduped: u64,
+    status: i32,
+    reserved: u32,
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct file_dedupe_range
+{
+    src_offset: u64,
+    src_length: u64,
+    dest_count: u16, //count of elements in the info array
+    reserved1: u16, //must be 0
+    reserved2: u16, //must be 0
+}
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct dedup
+{
+    args: file_dedupe_range,
+    info: file_dedupe_range_info,
+}
+
+const FIDEDUPERANGE: c_ulong = 0xC0189436; //copied from c. 0xC0: direction: (_IOC_READ|_IOC_WRITE). 0x18 size of struct file_dedupe_range. 0x94 type (see linux/fs.h). 0x36 number (see linux/fs.h)
+
+extern
+{
+    fn ioctl(fd: c_int, request: c_ulong, ...) -> c_int;
+}
 
 
 #[derive(Clone)]
@@ -91,6 +131,56 @@ fn main() -> std::io::Result<()>
                         matches += 1;
                         total_matchsize += matchlen;
                         skip_match_check = matchlen - 1;
+
+                        if matchlen >= 16
+                        {
+                            let file_src = File::open(&file_path)?;
+                            let src_fd: RawFd = file_src.as_raw_fd();
+                            let file_dest = File::options().write(true).open(&file_path)?;
+                            let dest_fd: RawFd = file_dest.as_raw_fd();
+                            let dest_fd_i64: i64 = dest_fd as i64;
+
+                            let mut dedup_request: dedup = dedup
+                            {
+                                args: file_dedupe_range
+                                {
+                                    src_offset: block_number_old*4096,
+                                    src_length: matchlen*4096,
+                                    dest_count: 1,
+                                    reserved1: 0,
+                                    reserved2: 0,
+                                },
+                                info: file_dedupe_range_info
+                                {
+                                    dest_fd: dest_fd_i64,
+                                    dest_offset: block_number*4096,
+                                    bytes_deduped: 0,
+                                    status: 0,
+                                    reserved: 0,
+                                },
+                            };
+
+                            let req = &mut dedup_request;
+
+                            let result: i32;
+                            unsafe
+                            {
+                                result = ioctl(src_fd, FIDEDUPERANGE, req as *mut _);
+                            }
+                            if result != 0
+                            {
+                                let errno_whatever = errno();
+                                let errno_i32: i32 = errno_whatever.0;
+                                println!("dedup error: ({}) {}", errno_i32, errno_whatever);
+                                panic!("aborting");
+                            }
+                            else
+                            {
+                                println!("dedup success!");
+                                println!("bytes_deduped {}", dedup_request.info.bytes_deduped);
+                                println!("status {}", dedup_request.info.status);
+                            }
+                        }
                     }
                 }
             }
@@ -144,6 +234,8 @@ fn check_match(file_path: &String, block_old: u64, block_new: u64, full_block_co
     let matchlen_max: u64 = block_new-block_old; //in blocks
 
     //match blocks before the first matching block found by its hash. only relevant in case of a hash collisions
+    /*
+    TODO: this cannot be used currently because the function calling this relies on the match starting at the passed position.
     let mut max_blocks_before: u64 = matchlen_max;
     if max_blocks_before > block_old-1
     {
@@ -174,7 +266,7 @@ fn check_match(file_path: &String, block_old: u64, block_new: u64, full_block_co
         {
             break;
         }
-    }
+    }*/
 
 
     //find additional matching blocks after the matching block found by its hash.
